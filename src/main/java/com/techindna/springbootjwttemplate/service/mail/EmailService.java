9295bf7d44f325d5865119e018629c897da4ece4
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-22 19:44:25 +0300

    feat: add EmailService interface

---
+++ src/main/java/com/techindna/springbootjwttemplate/service/mail/EmailService.java
@@ -0,0 +1,9 @@
+package com.techindna.springbootjwttemplate.service.mail;
+
+import com.techindna.springbootjwttemplate.entity.email.EmailDetails;
+
+public interface EmailService {
+
+    void sendMail(EmailDetails details);
+    void sendMailWithAttachment(EmailDetails details);
+}
