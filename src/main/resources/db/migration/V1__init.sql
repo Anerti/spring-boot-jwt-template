93edbc14f4f908f6924ce5db67651e49a5547120
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-23 10:47:09 +0300

    migration: reduce username VARCHAR to 50

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -2,7 +2,7 @@ CREATE TYPE IF NOT EXISTS user_role AS ENUM ('admin', 'customer');
 
 CREATE TABLE IF NOT EXISTS "user" (
     id         UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
-    username   VARCHAR(100) NOT NULL UNIQUE,
+    username   VARCHAR(50) NOT NULL UNIQUE,
     password   VARCHAR(255) NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name  VARCHAR(100) NOT NULL,
