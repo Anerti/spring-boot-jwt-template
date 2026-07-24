1bbf0ab7a978d3f979603fb3efdbb284b037242f
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-24 11:10:59 +0300

    feat: add GET /auth/verification endpoint

--- src/main/java/com/techindna/springbootjwttemplate/controller/AuthController.java
+++ src/main/java/com/techindna/springbootjwttemplate/controller/AuthController.java
@@ -2,13 +2,16 @@ package com.techindna.springbootjwttemplate.controller;
 
 import com.techindna.springbootjwttemplate.dto.MessageBody;
 import com.techindna.springbootjwttemplate.dto.RegisterInput;
+import com.techindna.springbootjwttemplate.dto.VerifyRegistrationResponse;
 import com.techindna.springbootjwttemplate.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +24,11 @@ public class AuthController {
     @PostMapping("/register")
     public ResponseEntity<MessageBody> register(@RequestBody RegisterInput request) {
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(authService.register(request));
+    }
+
+    @GetMapping("/verification")
+    public ResponseEntity<VerifyRegistrationResponse> verify(@RequestParam String code,
+                                                             @RequestParam String email) {
+        return ResponseEntity.status(HttpStatus.OK).body(authService.verify(code, email));
     }
 }
