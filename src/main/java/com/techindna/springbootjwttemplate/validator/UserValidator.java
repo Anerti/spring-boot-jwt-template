7acd1dd1303e3e00a569cf7e5a468601a9f6181e
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-07-24 14:10:42 +0300

    feat: add validateLogin to UserValidator

--- src/main/java/com/techindna/springbootjwttemplate/validator/UserValidator.java
+++ src/main/java/com/techindna/springbootjwttemplate/validator/UserValidator.java
@@ -1,5 +1,6 @@
 package com.techindna.springbootjwttemplate.validator;
 
+import com.techindna.springbootjwttemplate.dto.LoginInput;
 import com.techindna.springbootjwttemplate.dto.RegisterInput;
 import com.techindna.springbootjwttemplate.exception.http.UnprocessableContentException;
 import lombok.RequiredArgsConstructor;
@@ -10,6 +11,15 @@ import org.springframework.stereotype.Component;
 public class UserValidator {
 
     private final DataValidator dataValidator;
+
+    public void validateLogin(LoginInput request) {
+        dataValidator.checkNullData("password", request.getPassword());
+
+        if ((request.getUsername() == null || request.getUsername().isBlank())
+                && (request.getEmail() == null || request.getEmail().isBlank())) {
+            throw new UnprocessableContentException("Username or email is required and cannot be blank");
+        }
+    }
 
     public void validateRegistration(RegisterInput request) {
 
