8ce55bd3996f3207df9b4369e5d54e6475d0677e
Anerti <e.andrianombana@protonmail.com>
2026-08-24 12:33:42 +0300

    refactor: switch /users update to PATCH

--- src/main/java/com/techindna/springbootjwttemplate/controller/UserController.java
+++ src/main/java/com/techindna/springbootjwttemplate/controller/UserController.java
@@ -8,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +26,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(userService.getUserById(userId));
     }
 
-    @PutMapping("/{userId}")
+    @PatchMapping("/{userId}")
     public ResponseEntity<User> updateUser(
             @PathVariable UUID userId,
             @RequestBody UpdateUserInput input) {
