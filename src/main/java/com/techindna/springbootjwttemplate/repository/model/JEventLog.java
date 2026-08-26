a66d9a92815d61705e43d7e0bfa9908b0842b6f7
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-08-26 14:56:33 +0300

    add JEventLog JPA entity with @ManyToOne host

--- src/main/java/com/techindna/springbootjwttemplate/repository/model/JEventLog.java
+++ src/main/java/com/techindna/springbootjwttemplate/repository/model/JEventLog.java
@@ -1,22 +1,15 @@
 package com.techindna.springbootjwttemplate.repository.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import java.time.Instant;
-import java.util.UUID;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Entity
 @Table(name = "event_log")
@@ -31,7 +24,7 @@ public class JEventLog {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "host_id", nullable = false)
     private JHost host;
 
