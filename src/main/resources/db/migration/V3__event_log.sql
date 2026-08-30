859b89c846bd1cac7695a27dea8bb2766e9f8a5a
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-08-30 10:59:26 +0300

    feat: add status enum + column to event_log DDL

--- src/main/resources/db/migration/V3__event_log.sql
+++ src/main/resources/db/migration/V3__event_log.sql
@@ -1,11 +1,16 @@
+DO $$ BEGIN
+    CREATE TYPE jwt_template_app.event_log_status AS ENUM ('INFO', 'APPROVED', 'SECURITY', 'WARNING');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
 CREATE TABLE IF NOT EXISTS jwt_template_app.event_log (
         id         UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
         host_id    UUID        NOT NULL,
+        status     jwt_template_app.event_log_status NOT NULL DEFAULT 'INFO',
         description VARCHAR(100),
         created_at TIMESTAMPTZ  NOT NULL DEFAULT now(),
         CONSTRAINT fk_host_id FOREIGN KEY (host_id)
         REFERENCES jwt_template_app.host(id)
         ON DELETE CASCADE
     );
-
-
