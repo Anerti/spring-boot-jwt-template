0d6b854d6445a8d610b314f91df38f702a618f06
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-09-01 16:38:25 +0300

    test: configure testcontainers for PostgreSQL and Redis

--- src/test/java/com/techindna/springbootjwttemplate/SpringBootJwtTemplateApplicationTests.java
+++ src/test/java/com/techindna/springbootjwttemplate/SpringBootJwtTemplateApplicationTests.java
@@ -1,9 +1,14 @@
 package com.techindna.springbootjwttemplate;
 
+import com.techindna.springbootjwttemplate.config.TestcontainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@Import(TestcontainersConfig.class)
+@ActiveProfiles("test")
 class SpringBootJwtTemplateApplicationTests {
 
     @Test
---
+++ src/test/resources/test-schema.sql
@@ -0,0 +1,1 @@
+CREATE SCHEMA IF NOT EXISTS jwt_template_app;
