77a7277523b87bb49012f518cffc0076282b1166
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-09-01 19:01:34 +0300

    test: add test dependencies and mock GeoIP reader for context load

--- src/test/java/com/techindna/springbootjwttemplate/SpringBootJwtTemplateApplicationTests.java
+++ src/test/java/com/techindna/springbootjwttemplate/SpringBootJwtTemplateApplicationTests.java
@@ -1,15 +1,19 @@
 package com.techindna.springbootjwttemplate;
 
+import com.maxmind.geoip2.DatabaseReader;
 import com.techindna.springbootjwttemplate.config.TestcontainersConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 @Import(TestcontainersConfig.class)
 @ActiveProfiles("test")
 class SpringBootJwtTemplateApplicationTests {
+
+    @MockitoBean private DatabaseReader geoIpCityReader;
 
     @Test
     void contextLoads() {
